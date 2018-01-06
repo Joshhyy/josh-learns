d44e10fd4f3c2b6887635e803ee77feeb76ba418
Jacob Hackett <jacobrhackett@gmail.com>
2018-01-05 19:57:44 -0500

    adding a2

---
+++ a2/server.js
@@ -0,0 +1,28 @@
+var http = require('http')
+var fs = require('fs')
+var url = require('url')
+
+var server = http.createServer(function(req, res) {
+    var uri = url.parse(req.url)
+
+    switch(uri.pathname) {
+        case '/':
+            sendFile(res, 'index.html', 'text/html')
+            break
+        case '/style.css':
+            sendFile(res, 'style.css', 'text/css')
+            break
+        default:
+            res.end('404 not found')
+    }
+})
+
+server.listen(process.env.PORT || 8080)
+
+function sendFile(res, fileName, contentType) {
+    fs.readFile(fileName, function(err, content) {
+        res.writeHead(200, { 'Content-Type': contentType })
+        res.end(content, 'utf-8')
+    })
+}
+
